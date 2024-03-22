2c25311da5513db23af6f00e31d500a64a54d44c
YASSINE00e <yassineazzouz51@gmail.com>
2024-03-22 14:57:54 +0100

    feat: add Person/ Caregiver/ Patient models

---
+++ Models/Patient.cs
@@ -0,0 +1,62 @@
+﻿using System.Xml.Linq;
+
+namespace Viola.Models
+{
+    public class Patient : Person
+    {
+
+
+        private int age;
+        private string houseLocation;
+        private string bloodType;
+        private float weight;
+        private string currentLocation;
+        private int hm; 
+        private int mv;
+
+        
+        public Patient(int id, string name, string surname, int age, string houseLocation, string bloodType, float weight)
+            : base(id, name, surname)
+        {
+            this.age = age;
+            this.houseLocation = houseLocation;
+            this.bloodType = bloodType;
+            this.weight = weight;
+            
+        }
+
+        public void EditPatient(string _name, string _surname, int age, string houseLocation, string bloodType, float weight, string currentLocation, int hm, int mv)
+        {
+            Name = _name;
+            Surname = _surname;
+            this.age = age;
+            this.houseLocation = houseLocation;
+            this.bloodType = bloodType;
+            this.weight = weight;
+            this.currentLocation = currentLocation;
+            this.hm = hm;
+            this.mv = mv;
+        }
+        public int Age
+        {
+            get { return this.age; }
+            set { this.age = value; }
+        }
+        public string HouseLocation
+        {
+            get { return this.houseLocation; }
+            set { this.houseLocation = value; }
+        }
+        public string BloodType
+        {
+            get { return this.bloodType; }
+            set { this.bloodType = value;}
+        }
+        public float Weight
+        {
+            get { return this.weight; }
+            set { this.weight = value; }
+        }
+
+    }
+}
---
+++ Models/Person.cs
@@ -0,0 +1,33 @@
+﻿namespace Viola.Models
+{
+    public class Person
+    {
+        private int id;
+        private string name;
+        private string surname;
+
+        public Person(int id, string name, string surname)
+        {
+            this.id = id;
+            this.name = name;
+            this.surname = surname;
+        }
+
+        public int Id
+        {
+            get { return id; }
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public string Surname
+        {
+            get { return surname; }
+            set { surname = value; }
+        }
+    }
+}
