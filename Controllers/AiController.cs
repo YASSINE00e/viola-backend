8f7a02032cbab7685d87dee49c0e12e2f398dff3
YASSINE00e <yassineazzouz51@gmail.com>
2024-04-02 13:29:04 +0100

    feat: Add AiController.

---
+++ Controllers/AiController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using OpenAI_API;
+using OpenAI_API.Completions;
+using Viola.Models;
+
+namespace Viola.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AiController : ControllerBase
+    {/*
+        [HttpPost]
+        public async Task<IActionResult> Analysis(Patient patient)
+        {
+            string prompt = $"I'm a {patient.Age}-year-old person, my movement is {patient.Mv}, and my heart rate is {patient.Hm}. Give me a small analysis of my body situation in a maximum of 15 words.";
+            string answer = string.Empty;
+            var openai = new OpenAIAPI("sk - JsNWZkygoR5L61pddFHjT3BlbkFJSrJ8Pgsr17ateSHXrAIY");
+            CompletionRequest completion = new CompletionRequest();
+            completion.Prompt = prompt;
+            completion.Model = OpenAI_API.Models.Model.DavinciText;
+            completion.MaxTokens = 200;
+
+            var result = openai.Completions.CreateCompletionsAsync(completion);
+            foreach (var item in result.Result.Completions)
+            {
+                answer = item.Text;
+            }
+            return Ok(answer);
+        }*/
+
+        [HttpPost]
+        public async Task<IActionResult> Quote()
+        {
+            try
+            {
+                string prompt = "Give me a quote/information about Alzheimer's";
+                string answer = string.Empty;
+                var openai = new OpenAIAPI("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");
+                CompletionRequest completion = new CompletionRequest();
+                completion.Prompt = prompt;
+                completion.Model = OpenAI_API.Models.Model.DavinciText;
+                completion.MaxTokens = 200;
+                var result = await openai.Completions.CreateCompletionsAsync(completion);
+                if (result?.Completions != null && result.Completions.Any())
+                {
+                    answer = result.Completions.First().Text;
+                }
+
+                return Ok(answer);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, "An error occurred: " + ex.Message);
+            }
+        }
+
+
+    }
+}
--- Models/Patient.cs
+++ Models/Patient.cs
@@ -12,20 +12,21 @@ namespace Viola.Models
         private float weight;
         private string currentLocation;
         private int hm; 
-        private int mv;
+        private string mv;
 
         
-        public Patient(int id, string name, string surname, int age, string houseLocation, string bloodType, float weight)
+        public Patient(int id, string name, string surname, int age, string houseLocation, string bloodType, float weight, int hm, string mv)
             : base(id, name, surname)
         {
             this.age = age;
             this.houseLocation = houseLocation;
             this.bloodType = bloodType;
             this.weight = weight;
-            
+            this.hm = hm;
+            this.mv = mv;
         }
 
-        public void EditPatient(string _name, string _surname, int age, string houseLocation, string bloodType, float weight, string currentLocation, int hm, int mv)
+        public void EditPatient(string _name, string _surname, int age, string houseLocation, string bloodType, float weight, string currentLocation, int hm, string mv)
         {
             Name = _name;
             Surname = _surname;
@@ -56,6 +57,16 @@ namespace Viola.Models
         {
             get { return this.weight; }
             set { this.weight = value; }
+        }
+        public int Hm
+        {
+            get { return this.hm; }
+            set { this.hm = value; }
+        }
+        public string Mv
+        {
+            get { return this.mv; }
+            set { this.mv = value; }
         }
 
     }
