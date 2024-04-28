74b02dc49c151fff63abbde01d5ff53deda1476a
YASSINE00e <yassineazzouz51@gmail.com>
2024-04-28 16:21:53 +0100

    feat: Add an AddPatientApiModel & fix the PatientController .

--- Controllers/PatientController.cs
+++ Controllers/PatientController.cs
@@ -1,8 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using MySql.Data.MySqlClient;
-using MySqlX.XDevAPI.Common;
-using System.Data;
 using Viola.Models;
 
 namespace Viola.Controllers
@@ -18,11 +15,12 @@ namespace Viola.Controllers
         }
 
         [HttpGet]
-        public IActionResult Get()
+        [Route("GetPatients")]
+        public IActionResult GetPatients(int id)
         {
             try
             {
-                string query = @"select * from patient";
+                string query = @"SELECT * FROM patient, relation WHERE patient.ViolaId = relation.ViolaId AND relation.idcaregiver = @id";
                 List<Dictionary<string, object>> result = new List<Dictionary<string, object>>();
                 string sqlDataSource = _configuration.GetConnectionString("ViolaDBCon");
 
@@ -31,6 +29,7 @@ namespace Viola.Controllers
                     cnn.Open();
                     using (MySqlCommand cmd = new MySqlCommand(query, cnn))
                     {
+                        cmd.Parameters.Add("@id", MySqlDbType.Int32).Value = id;
                         using (MySqlDataReader myReader = cmd.ExecuteReader())
                         {
                             while (myReader.Read())
@@ -45,111 +44,55 @@ namespace Viola.Controllers
                         }
                     }
                 }
-                return Ok(result);
+                return Ok(new { status = 200, data = result, nbPatients = result.Count });  //200 OK
             }
             catch (Exception ex)
             {
-                return StatusCode(500, ex.Message);
+                return StatusCode(500, new { message = ex.Message }); // 500 (Internal Server Error)
             }
         }
 
 
 
         [HttpPost]
-        public IActionResult Post(Patient patient)
+        [Route("AddPatient")]
+        public IActionResult AddPatient(AddPatientApiModel patient)
         {
             try
             {
-                string query = @"INSERT INTO patient (Name, Surname, Age, HouseLocation, BloodType, Weight)
-                     VALUES (@PatientName, @PatientSurname, @PatientAge, @PatientHouseLocation, @PatientBloodType, @PatientWeight)";
                 string sqlDataSource = _configuration.GetConnectionString("ViolaDBCon");
-
                 using (MySqlConnection cnn = new MySqlConnection(sqlDataSource))
                 {
+                    cnn.Open();
+                    string query = @"INSERT INTO patient (ViolaId, Name, Surname, Age, HouseLocation, BloodType, Weight)
+                     VALUES (@ViolaId, @PatientName, @PatientSurname, @PatientAge, @PatientHouseLocation, @PatientBloodType, @PatientWeight)";
                     using (MySqlCommand cmd = new MySqlCommand(query, cnn))
                     {
+                        cmd.Parameters.Add("@ViolaId", MySqlDbType.Int32).Value = patient.ViolaId;
                         cmd.Parameters.Add("@PatientName", MySqlDbType.VarChar).Value = patient.Name;
                         cmd.Parameters.Add("@PatientSurname", MySqlDbType.VarChar).Value = patient.Surname;
                         cmd.Parameters.Add("@PatientAge", MySqlDbType.Int32).Value = patient.Age;
                         cmd.Parameters.Add("@PatientHouseLocation", MySqlDbType.VarChar).Value = patient.HouseLocation;
                         cmd.Parameters.Add("@PatientBloodType", MySqlDbType.VarChar).Value = patient.BloodType;
                         cmd.Parameters.Add("@PatientWeight", MySqlDbType.Float).Value = patient.Weight;
-                        cnn.Open();
                         cmd.ExecuteNonQuery();
-                        cnn.Close();
                     }
-                }
-                return Ok("Added Successfully");
-            }
-            catch (Exception ex)
-            {
-                return StatusCode(500, ex.Message);
-            }
 
-        }
 
-        [HttpPut]
-        public IActionResult Put(Patient patient)
-        {
-            try
-            {
-                string query = @"update patient set
-                            Name = @PatientName, Surname = @PatientSurname,
-                            Age = @PatientAge, HouseLocation =@PatientHouseLocation,  
-                            BloodType = @PatientBloodType, Weight = @PatientWeight
-                            where idPatient = @PatientId";
-                string sqlDataSource = _configuration.GetConnectionString("ViolaDBCon");
-
-                using (MySqlConnection cnn = new MySqlConnection(sqlDataSource))
-                {
-                    using (MySqlCommand cmd = new MySqlCommand(query, cnn))
+                    string relationQuery = @"INSERT INTO relation (idCaregiver, ViolaId) VALUES (@idCaregiver, @ViolaId)";
+                    using (MySqlCommand cmd = new MySqlCommand(relationQuery, cnn))
                     {
-                        cmd.Parameters.Add("@PatientId", MySqlDbType.Int32).Value = patient.Id;
-                        cmd.Parameters.Add("@PatientName", MySqlDbType.VarChar).Value = patient.Name;
-                        cmd.Parameters.Add("@PatientSurname", MySqlDbType.VarChar).Value = patient.Surname;
-                        cmd.Parameters.Add("@PatientAge", MySqlDbType.Int32).Value = patient.Age;
-                        cmd.Parameters.Add("@PatientHouseLocation", MySqlDbType.VarChar).Value = patient.HouseLocation;
-                        cmd.Parameters.Add("@PatientBloodType", MySqlDbType.VarChar).Value = patient.BloodType;
-                        cmd.Parameters.Add("@PatientWeight", MySqlDbType.Float).Value = patient.Weight;
-                        cnn.Open();
+                        cmd.Parameters.Add("@idCaregiver", MySqlDbType.Int32).Value = patient.caregiverId;
+                        cmd.Parameters.Add("@ViolaId", MySqlDbType.Int32).Value = patient.ViolaId;
                         cmd.ExecuteNonQuery();
-                        cnn.Close();
                     }
+                    cnn.Close();
                 }
-                return Ok("Updated Successfully");
+                return Ok(new { status = 200, message = "Added Successfully" }); // 200 (OK)
             }
             catch (Exception ex)
             {
-                return StatusCode(500, ex.Message);
-            }
-
-        }
-
-        [HttpDelete("{id}")]
-        public IActionResult Delete(int id)
-        {
-            try
-            {
-                string query = @"delete from patient
-                            where idPatient = @PatientId";
-                string sqlDataSource = _configuration.GetConnectionString("ViolaDBCon");
-
-                using (MySqlConnection cnn = new MySqlConnection(sqlDataSource))
-                {
-                    using (MySqlCommand cmd = new MySqlCommand(query, cnn))
-                    {
-                        cmd.Parameters.Add("@PatientId", MySqlDbType.Int32).Value = id;
-                        cnn.Open();
-                        cmd.ExecuteNonQuery();
-                        cnn.Close();
-
-                    }
-                }
-                return Ok("Deleted Successfully");
-            }
-            catch (Exception ex)
-            {
-                return StatusCode(500, ex.Message);
+                return StatusCode(500, new { message = ex.Message }); // 500 (Internal Server Error)
             }
 
         }
---
+++ Models/AddPatientApiModel.cs
@@ -0,0 +1,14 @@
+﻿namespace Viola.Models
+{
+    public class AddPatientApiModel
+    {
+        public int ViolaId { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int Age { get; set; }
+        public string HouseLocation { get; set; }
+        public string BloodType { get; set; }
+        public float Weight { get; set; }
+        public int caregiverId { get; set; }
+    }
+}
