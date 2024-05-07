19bfc0366ef2ac775087b40d80bdea06b29efcf2
YASSINE00e <yassineazzouz51@gmail.com>
2024-05-07 19:37:10 +0100

    feat: Add user functionality to change data.

--- Controllers/SettingsController.cs
+++ Controllers/SettingsController.cs
@@ -14,158 +14,15 @@ namespace Viola.Controllers
             _configuration = configuration;
         }
 
-        [HttpPut]
-        [Route("ChangeName")]
-        public IActionResult ChangeName(int id, string name)
+
+        [HttpGet]
+        [Route("GetUserData")]
+        public IActionResult GetUserData(int id)
         {
             try
             {
-                string query = @"update caregiver set Name = @CaregiverName where idCareGiver = @CaregiverId";
-                string sqlDataSource = _configuration.GetConnectionString("ViolaDBCon");
-
-                using (MySqlConnection cnn = new MySqlConnection(sqlDataSource))
-                {
-                    using (MySqlCommand cmd = new MySqlCommand(query, cnn))
-                    {
-                        cmd.Parameters.Add("@CaregiverId", MySqlDbType.Int32).Value = id;
-                        cmd.Parameters.Add("@CaregiverName", MySqlDbType.VarChar).Value = name;
-                        cnn.Open();
-                        cmd.ExecuteNonQuery();
-                        cnn.Close();
-                    }
-                }
-                return Ok("Updated Successfully");
-            }
-            catch (Exception ex)
-            {
-                return StatusCode(500, ex.Message);
-            }
-
-        }
-
-        [HttpPut]
-        [Route("ChangeSurname")]
-        public IActionResult ChangeSurname(int id, string surname)
-        {
-            try
-            {
-                string query = @"update caregiver set Name = @CaregiverName where idCareGiver = @CaregiverId";
-                string sqlDataSource = _configuration.GetConnectionString("ViolaDBCon");
-
-                using (MySqlConnection cnn = new MySqlConnection(sqlDataSource))
-                {
-                    using (MySqlCommand cmd = new MySqlCommand(query, cnn))
-                    {
-                        cmd.Parameters.Add("@CaregiverId", MySqlDbType.Int32).Value = id;                        
-                        cmd.Parameters.Add("@CaregiverSurname", MySqlDbType.VarChar).Value = surname;
-                        cnn.Open();
-                        cmd.ExecuteNonQuery();
-                        cnn.Close();
-                    }
-                }
-                return Ok("Updated Successfully");
-            }
-            catch (Exception ex)
-            {
-                return StatusCode(500, ex.Message);
-            }
-
-        }
-
-        [HttpPut]
-        [Route("ChangePhone")]
-        public IActionResult ChangePhone(int id, string phone)
-        {
-            try
-            {
-                string query = @"update caregiver set Name = @CaregiverName where idCareGiver = @CaregiverId";
-                string sqlDataSource = _configuration.GetConnectionString("ViolaDBCon");
-
-                using (MySqlConnection cnn = new MySqlConnection(sqlDataSource))
-                {
-                    using (MySqlCommand cmd = new MySqlCommand(query, cnn))
-                    {
-                        cmd.Parameters.Add("@CaregiverId", MySqlDbType.Int32).Value = id;
-                        cmd.Parameters.Add("@CaregiverPhone", MySqlDbType.VarChar).Value = phone;
-                        cnn.Open();
-                        cmd.ExecuteNonQuery();
-                        cnn.Close();
-                    }
-                }
-                return Ok("Updated Successfully");
-            }
-            catch (Exception ex)
-            {
-                return StatusCode(500, ex.Message);
-            }
-
-        }
-
-        [HttpPut]
-        [Route("ChangeMail")]
-        public IActionResult ChangeMail(int id, string mail)
-        {
-            try
-            {
-                string query = @"update caregiver set Name = @CaregiverName where idCareGiver = @CaregiverId";
-                string sqlDataSource = _configuration.GetConnectionString("ViolaDBCon");
-
-                using (MySqlConnection cnn = new MySqlConnection(sqlDataSource))
-                {
-                    using (MySqlCommand cmd = new MySqlCommand(query, cnn))
-                    {
-                        cmd.Parameters.Add("@CaregiverId", MySqlDbType.Int32).Value = id;
-                        cmd.Parameters.Add("@CaregiverMail", MySqlDbType.VarChar).Value = mail;
-                        cnn.Open();
-                        cmd.ExecuteNonQuery();
-                        cnn.Close();
-                    }
-                }
-                return Ok("Updated Successfully");
-            }
-            catch (Exception ex)
-            {
-                return StatusCode(500, ex.Message);
-            }
-
-        }
-
-        [HttpPut]
-        [Route("ChangePassword")]
-        public IActionResult ChangePassword(int id, string password)
-        {
-            try
-            {
-                string query = @"update caregiver set Name = @CaregiverName where idCareGiver = @CaregiverId";
-                string sqlDataSource = _configuration.GetConnectionString("ViolaDBCon");
-
-                using (MySqlConnection cnn = new MySqlConnection(sqlDataSource))
-                {
-                    using (MySqlCommand cmd = new MySqlCommand(query, cnn))
-                    {
-                        cmd.Parameters.Add("@CaregiverId", MySqlDbType.Int32).Value = id;
-                        cmd.Parameters.Add("@CaregiverPassword", MySqlDbType.VarChar).Value = password;
-                        cnn.Open();
-                        cmd.ExecuteNonQuery();
-                        cnn.Close();
-                    }
-                }
-                return Ok("Updated Successfully");
-            }
-            catch (Exception ex)
-            {
-                return StatusCode(500, ex.Message);
-            }
-
-        }
-
-        [Route("DeleteUser")]
-        [HttpDelete("{id}")]
-        public IActionResult DeleteUser(int id)
-        {
-            try
-            {
-                string query = @"delete from caregiver where idCareGiver = @CaregiverId";
+                SettingsApiModel caregiver = new SettingsApiModel();
+                string query = @"select * from caregiver where idCareGiver = @CaregiverId";
                 string sqlDataSource = _configuration.GetConnectionString("ViolaDBCon");
 
                 using (MySqlConnection cnn = new MySqlConnection(sqlDataSource))
@@ -174,11 +31,63 @@ namespace Viola.Controllers
                     {
                         cmd.Parameters.Add("@CaregiverId", MySqlDbType.Int32).Value = id;
                         cnn.Open();
-                        cmd.ExecuteNonQuery();
+                        using (MySqlDataReader reader = cmd.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                caregiver.Name = reader["Name"].ToString();
+                                caregiver.Surname = reader["Surname"].ToString();
+                                caregiver.Phone = reader["Phone"].ToString();
+                                caregiver.Mail = reader["Mail"].ToString();
+                                caregiver.Password = reader["Password"].ToString();
+                            }
+                        }
                         cnn.Close();
                     }
                 }
-                return Ok("Deleted Successfully");
+                if (caregiver != null)
+                {
+                    return Ok(new { status = 200, data = caregiver });
+                }
+                else
+                {
+                    return NotFound(new { status = 404, data = caregiver });
+                }
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+
+        [HttpPost]
+        [Route("ChangeData")]
+        public IActionResult ChangeData(ChangeDataApiModel caregiver)
+        {
+            try
+            {
+                string query = @"update caregiver set
+                            Name = @CaregiverName, Surname = @CaregiverSurname,
+                            Phone = @CaregiverPhone, Mail =@CaregiverMail,  Password = @CaregiverPassword
+                            where idCareGiver = @CaregiverId";
+                string sqlDataSource = _configuration.GetConnectionString("ViolaDBCon");
+
+                using (MySqlConnection cnn = new MySqlConnection(sqlDataSource))
+                {
+                    cnn.Open();
+                    using (MySqlCommand cmd = new MySqlCommand(query, cnn))
+                    {
+                        cmd.Parameters.Add("@CaregiverId", MySqlDbType.Int32).Value = caregiver.CaregiverId;
+                        cmd.Parameters.Add("@CaregiverName", MySqlDbType.VarChar).Value = caregiver.Name;
+                        cmd.Parameters.Add("@CaregiverSurname", MySqlDbType.VarChar).Value = caregiver.Surname;
+                        cmd.Parameters.Add("@CaregiverPhone", MySqlDbType.VarChar).Value = caregiver.Phone;
+                        cmd.Parameters.Add("@CaregiverMail", MySqlDbType.VarChar).Value = caregiver.Mail;
+                        cmd.Parameters.Add("@CaregiverPassword", MySqlDbType.VarChar).Value = caregiver.Password;
+                        cmd.ExecuteNonQuery();
+                    }
+                    cnn.Close();
+                }
+                return Ok(new { status = 200, message = "Updated" });
             }
             catch (Exception ex)
             {
@@ -186,5 +95,7 @@ namespace Viola.Controllers
             }
 
         }
+
+
     }
 }
---
+++ Models/ChangeDataApiModel.cs
@@ -0,0 +1,12 @@
+﻿namespace Viola.Models
+{
+    public class ChangeDataApiModel
+    {
+        public int CaregiverId { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Phone { get; set; }
+        public string Mail { get; set; }
+        public string Password { get; set; }
+    }
+}
